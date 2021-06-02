d1811544bdf317a1383dbb7dcb7214574ad2cc55
Taichi Sato <info@syarihu.net>
2021-06-02 21:49:00 +0900

    Added isEmpty method because TextUtils.isEmpty is used in the constructor of SkuDetails

--- library/src/test/java/android/text/TextUtils.java
+++ library/src/test/java/android/text/TextUtils.java
@@ -27,4 +27,13 @@ public class TextUtils {
     }
     return false;
   }
+
+  /**
+   * Returns true if the string is null or 0-length.
+   * @param str the string to be examined
+   * @return true if str is null or zero length
+   */
+  public static boolean isEmpty(CharSequence str) {
+    return str == null || str.length() == 0;
+  }
 }
